6787a06cc3f7e3856954ffb1cfe2e4f0f09a1755
Raj Pathak <rajpathak836@gmail.com>
2026-01-24 16:58:05 +0530

    Delete kbc_game.py

--- kbc_game.py
+++
@@ -1,7 +0,0 @@
-# KBC Game Sound Script
-# This script plays a sound file using the playsound library
-
-from playsound import playsound
-
-# Play sound (make sure the mp3 file is in the same folder)
-playsound("Kbc_Clock.mp3")
